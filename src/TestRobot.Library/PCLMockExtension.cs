806e48444b18e6e332b9b7edd54aad942bb7500b
Tobias Buchholz <tobias.buchholz89@gmail.com>
2021-09-06 22:02:51 +0200

    add documentation to extension methods

--- src/TestRobot.Library/PCLMockExtension.cs
+++ src/TestRobot.Library/PCLMockExtension.cs
@@ -6,31 +6,97 @@ using PCLMock;
 
 namespace TestRobot
 {
+	/// <summary>
+	/// Extension methods to facilitate setting up the <see cref="WhenContinuation{TMock, TMember}"/> objects of a mocked object.
+	/// </summary>
 	public static class PCLMockExtension
 	{
+		/// <summary>
+		/// Requests that the given exception will be thrown if the member is accessed.
+		/// </summary>
+		/// <param name="this">
+		/// The object the method gets invoked on.
+		/// </param>
+		/// <param name="e">
+		/// The exception that should be thrown.
+		/// </param>
+		/// <typeparam name="TMock">
+		/// The type of the object being mocked.
+		/// </typeparam>
+		/// <typeparam name="TMember">
+		/// The type being returned by the member being specified.
+		/// </typeparam>
 		public static void ThrowObservable<TMock, TMember>(this WhenContinuation<TMock, IObservable<TMember>> @this, Exception e) 
 		{
 			@this.Return(Observable.Throw<TMember>(e));
 		}
 
-		public static void ReturnsObservable<TMock, TMember>(this WhenContinuation<TMock, IObservable<TMember>> @this, TMember member) 
+		/// <summary>
+		/// Requests that a specified value will be returned as observable if the member is accessed. 
+		/// </summary>
+		/// <param name="this">
+		/// The object the method gets invoked on.
+		/// </param>
+		/// <param name="value">
+		/// The value to return.
+		/// </param>
+		/// <typeparam name="TMock">
+		/// The type of the object being mocked.
+		/// </typeparam>
+		/// <typeparam name="TMember">
+		/// The type being returned by the member being specified.
+		/// </typeparam>
+		public static void ReturnsObservable<TMock, TMember>(this WhenContinuation<TMock, IObservable<TMember>> @this, TMember value) 
 		{
-			@this.Return(Observable.Return(member));
+			@this.Return(Observable.Return(value));
 		}
 
-        public static void ReturnsSignal<TMock>(this WhenContinuation<TMock, IObservable<Unit>> @this) 
+        /// <summary>
+        /// Requests that a unit as observable will be returned if the member is accessed.
+        /// </summary>
+		/// <param name="this">
+		/// The object the method gets invoked on.
+		/// </param>
+		/// <typeparam name="TMock">
+		/// The type of the object being mocked.
+		/// </typeparam>
+        public static void ReturnsUnit<TMock>(this WhenContinuation<TMock, IObservable<Unit>> @this) 
         {
             @this.Return(Observable.Return(Unit.Default));
         }
 
-		public static void ReturnsAsync<TMock, TMember>(this WhenContinuation<TMock, Task<TMember>> @this, TMember member) 
+		/// <summary>
+        /// Requests that a specified value will be returned as task if the member is accessed.
+		/// </summary>
+		/// <param name="this">
+		/// The object the method gets invoked on.
+		/// </param>
+		/// <param name="value">
+		/// The value to return.
+		/// </param>
+		/// <typeparam name="TMock">
+		/// The type of the object being mocked.
+		/// </typeparam>
+		/// <typeparam name="TMember">
+		/// The type being returned by the member being specified.
+		/// </typeparam>
+		public static void ReturnsAsync<TMock, TMember>(this WhenContinuation<TMock, Task<TMember>> @this, TMember value) 
 		{
-			@this.Return(Task.FromResult(member));
+			@this.Return(Task.FromResult(value));
 		}
 
+		/// <summary>
+        /// Requests that a completed task will be returned if the member is accessed.
+		/// </summary>
+		/// <param name="this">
+		/// The object the method gets invoked on.
+		/// </param>
+		/// <typeparam name="TMock">
+		/// The type of the object being mocked.
+		/// </typeparam>
 		public static void ReturnsAsync<TMock>(this WhenContinuation<TMock, Task> @this) 
 		{
-			@this.Return(Task.FromResult(1));
+			@this.Return(Task.CompletedTask);
 		}
 	}
 }
--- src/TestRobot.Library/VerifyContinuationExtension.cs
+++ src/TestRobot.Library/VerifyContinuationExtension.cs
@@ -4,8 +4,26 @@ using PCLMock;
 
 namespace TestRobot
 {
+	/// <summary>
+	/// Extension methods for the mocked object that return a <see cref="RobotVerifyContinuation{TRobot, TRobotResult}"/> so the calls can be chained.
+	/// </summary>
 	public static class VerifyContinuationExtension
 	{
+		/// <summary>
+		/// Extension method that begins a verification specification.
+		/// </summary>
+		/// <param name="mock">
+		/// The mock on which the method gets called.
+		/// </param>
+		/// <param name="robotResult">
+		/// The robot result object that enables the chaining of calls.
+		/// </param>
+		/// <param name="selector">
+		/// An expression that resolves to the member being verified.
+		/// </param>
+		/// <returns>
+		/// A continuation object with which the verification can be specified.
+		/// </returns>
 		public static RobotVerifyContinuation<TRobot, TRobotResult> Verify<T, TRobot, TRobotResult>(
             this MockBase<T> mock,
             TestRobotResultBase<TRobot, TRobotResult> robotResult,
@@ -16,7 +34,30 @@ namespace TestRobot
 			return new RobotVerifyContinuation<TRobot, TRobotResult>(mock.Verify(selector), robotResult);
 		}
 
-        public static RobotVerifyContinuation<TRobot, TRobotResult> Verify<T, TMember, TRobot, TRobotResult>(
+		/// <summary>
+		/// Extension method that begins a verification specification.
+		/// </summary>
+		/// <typeparam name="T">
+		/// The type being mocked.
+		/// </typeparam>
+		/// <typeparam name="TMember">
+		/// The type of the property.
+		/// </typeparam>
+		/// <typeparam name="TRobot"></typeparam>
+		/// <typeparam name="TRobotResult"></typeparam>
+		/// <param name="mock">
+		/// The mock on which the method gets called.
+		/// </param>
+		/// <param name="robotResult">
+		/// The robot result object that enables the chaining of calls.
+		/// </param>
+		/// <param name="selector">
+		/// An expression that resolves to the member being verified.
+		/// </param>
+		/// <returns>
+		/// A continuation object with which the verification can be specified.
+		/// </returns>
+		public static RobotVerifyContinuation<TRobot, TRobotResult> Verify<T, TMember, TRobot, TRobotResult>(
             this MockBase<T> mock, 
             TestRobotResultBase<TRobot, TRobotResult> robotResult,
             Expression<Func<T, TMember>> selector)
@@ -26,7 +67,33 @@ namespace TestRobot
             return new RobotVerifyContinuation<TRobot, TRobotResult>(mock.Verify(selector), robotResult);
         }
 
-        public static RobotVerifyContinuation<TRobot, TRobotResult> VerifyPropertySet<T, TMember, TRobot, TRobotResult>(
+		/// <summary>
+		/// Extension method that begins a verification specification for a property set.
+		/// </summary>
+		/// <typeparam name="T">
+		/// The type being mocked.
+		/// </typeparam>
+		/// <typeparam name="TMember">
+		/// The type of the property.
+		/// </typeparam>
+		/// <typeparam name="TRobot"></typeparam>
+		/// <typeparam name="TRobotResult"></typeparam>
+		/// <param name="mock">
+		/// The mock on which the method gets called.
+		/// </param>
+		/// <param name="robotResult">
+		/// The robot result object that enables the chaining of calls.
+		/// </param>
+		/// <param name="propertySelector">
+		/// An expression that resolves to the property being verified.
+		/// </param>
+		/// <param name="valueFilterSelector">
+		/// An optional expression that can provide filtering against the property value being set.
+		/// </param>
+		/// <returns>
+		/// A continuation object with which the verification can be specified.
+		/// </returns>
+		public static RobotVerifyContinuation<TRobot, TRobotResult> VerifyPropertySet<T, TMember, TRobot, TRobotResult>(
             this MockBase<T> mock, 
             TestRobotResultBase<TRobot, TRobotResult> robotResult, 
             Expression<Func<T, TMember>> propertySelector, 
@@ -38,6 +105,9 @@ namespace TestRobot
         }
 	}
 
+	/// <summary>
+	/// Facilitates the expression of verifications against a member in a <see cref="MockBase{T}"/>.
+	/// </summary>
 	public sealed class RobotVerifyContinuation<TRobot, TRobotResult>
         where TRobot : TestRobotBase<TRobot, TRobotResult>
         where TRobotResult : TestRobotResultBase<TRobot, TRobotResult>
@@ -45,7 +115,7 @@ namespace TestRobot
 		private readonly VerifyContinuation _verifyContinuation;
 		private readonly TRobotResult _robotResult;
 
-		public RobotVerifyContinuation(
+		internal RobotVerifyContinuation(
 			VerifyContinuation verifyContinuation,
 			TestRobotResultBase<TRobot, TRobotResult> robotResult)
 		{
@@ -53,42 +123,72 @@ namespace TestRobot
 			_robotResult = (TRobotResult) robotResult;
 		}
 
+		/// <summary>
+		/// Verifies that the member was not called.
+		/// </summary>
 		public TRobotResult WasNotCalled()
 		{
 			_verifyContinuation.WasNotCalled();
 			return _robotResult;
 		}
 
+		/// <summary>
+		/// Verifies that the member called <paramref name="times"/> or fewer times.
+		/// </summary>
+		/// <param name="times">
+		/// The maximum number of times the member must have been called.
+		/// </param>
 		public TRobotResult WasCalledAtMost(int times)
 		{
 			_verifyContinuation.WasCalledAtMost(times);
 			return _robotResult;
 		}
 
+		/// <summary>
+		/// Verifies that the member was called <paramref name="times"/> or more times.
+		/// </summary>
+		/// <param name="times">
+		/// The minimum number of times the member must have been called.
+		/// </param>
 		public TRobotResult WasCalledAtLeast(int times)
 		{
 			_verifyContinuation.WasCalledAtLeast(times);
 			return _robotResult;
 		}
 
+		/// <summary>
+		/// Verifies that the member was called exactly <paramref name="times"/> time.
+		/// </summary>
+		/// <param name="times">
+		/// The number of times the member must have been called.
+		/// </param>
 		public TRobotResult WasCalledExactly(int times)
 		{
 			_verifyContinuation.WasCalledExactly(times);
 			return _robotResult;
 		}
 
+		/// <summary>
+		/// Verifies that the member was either not called, or only called once.
+		/// </summary>
 		public TRobotResult WasCalledAtMostOnce()
 		{
 			_verifyContinuation.WasCalledAtMostOnce();
 			return _robotResult;
 		}
 
+		/// <summary>
+		/// Verifies that the member was called exactly one time.
+		/// </summary>
 		public TRobotResult WasCalledExactlyOnce()
 		{
 			_verifyContinuation.WasCalledExactlyOnce();
 			return _robotResult;
 		}
 
+		/// <summary>
+		/// Verifies that the member was called one or more times.
+		/// </summary>
 		public TRobotResult WasCalledAtLeastOnce()
 		{
 			_verifyContinuation.WasCalledAtLeastOnce();
